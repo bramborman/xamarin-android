b92a5c2ef5e9e5532fd249966bc2817f1feae1a3
Marian Dolinský <mariandolinsky@outlook.com>
2021-11-25 20:27:24 +0100

    Android.Content.Context.GetSystemService<T>

--- src/Mono.Android/Android.Content/Context.cs
+++ src/Mono.Android/Android.Content/Context.cs
@@ -10,5 +10,10 @@ namespace Android.Content {
 			Intent intent = new Intent (this, type);
 			StartActivity (intent);
 		}
+
+		public void GetSystemService<T> (string name) where T : Java.Lang.Object
+		{
+			return (T) GetSystemService (name);
+		}
 	}
 }
